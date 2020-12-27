f0887a147595e6c5428cc6f0d5674e904313110f
MatsMulder95 <mats.mulder@live>
2020-12-27 15:45:30 +0100

    fix google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,11 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: ['@nuxtjs/color-mode'],
+  buildModules: [
+    '@nuxtjs/color-mode',
+    '@nuxtjs/google-analytics',
+
+  ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
